f218d2eb72597c8ba426cccf5aa3b327ee694e6e
Royaltie-byte <186587026+Royaltie-byte@users.noreply.github.com>
2026-06-18 11:20:42 +0300

    add whatsapp float button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Gallery from './components/sections/Gallery'
 import FindUs from './components/sections/FindUs'
 import Booking from './components/sections/Booking'
 import Footer from './components/layout/Footer'
+import WhatsAppFloat from './components/ui/WhatsappFloat'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
         <Booking />
       </main>
       <Footer />
+      <WhatsAppFloat/>
+      
     </>
   )
 }
